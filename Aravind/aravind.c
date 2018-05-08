0f61237d6125329c2f0909a72af8afce9830d6b6
bsauniv <aravindkumar1.sub@gmail.com>
2018-05-08 11:16:41 +0530

    auro

---
+++ Aravind/aravind.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ int a,b,c;
+  clrscr();
+  printf("Enter the numbers in A and B");
+  scanf("%d %d",&a,&b);
+  c=a*b;
+  printf("The multiplication of two numbers is %d",c);
+  getch();
+}
